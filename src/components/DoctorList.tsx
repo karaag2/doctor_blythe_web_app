7165b2fc5248f854859bcc3959078e2afcee4cea
Karaag2 <amosissa6@gmail.com>
2025-08-01 23:30:06 +0100

    fix

--- src/components/DoctorList.tsx
+++ src/components/DoctorList.tsx
@@ -7,13 +7,13 @@ const DoctorList = () => {
 	return (
 		<div className="bg-blue-100 py-8 text-center">
 			<DetailCard heading="team" title="our doctors" />
-			<div className="flex space-x-6 px-4 py-8 overflow-x-scroll">
+			<div className="flex space-x-6 px-4 py-8 overflow-x-scroll scrollbar-hide">
 				<DoctorCard name="Mamman Bo" speciality="Chirgueon" />
 				<DoctorCard name="Mamman Bo" speciality="Chirgueon" />
 				<DoctorCard name="Mamman Bo" speciality="Chirgueon" />
 				<DoctorCard name="Mamman Bo" speciality="Chirgueon" />
 			</div>
-			<div className="px-12">
+			<div className="mx-auto px-12 max-w-sm">
 				<Button title="See-All" addStyle="w-full" />
 			</div>
 		</div>
